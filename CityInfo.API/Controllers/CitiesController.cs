af5f8d646eba08852c4b7bfbcd88e6da52d2fa2f
mimski <maria.bg.1989@gmail.com>
2024-10-07 19:29:43 +0300

    initial

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers;
+
+[ApiController]
+[Route("api/cities")]
+public class CitiesController : ControllerBase
+{
+    [HttpGet]
+    public JsonResult GetCities() 
+    {
+        return new JsonResult(new List<object> { new { id = 1, Name = "Sofia" }, new { id = 2, Name = "Varna"}});
+    }
+}
