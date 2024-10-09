2fc6346ab68ec67fe9cf3dc9b4f8ad1688302c75
mimski <maria.bg.1989@gmail.com>
2024-10-09 18:00:16 +0300

    adds GetFile

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers;
+
+[Route("api/files")]
+[ApiController]
+public class FilesController : ControllerBase
+{
+    private readonly FileExtensionContentTypeProvider fileExtensionContentTypeProvider;
+
+    public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+    {
+        this.fileExtensionContentTypeProvider = fileExtensionContentTypeProvider ?? throw new ArgumentException(nameof(fileExtensionContentTypeProvider));
+    }
+
+    public ActionResult GetFile(string fileId)
+    {
+        //concept code
+        var pathToFile = "test.txt";
+
+        if (!System.IO.File.Exists(pathToFile))
+        {
+            return NotFound();
+        }
+
+        if (!fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType)) 
+        {
+            contentType = "application/octet-stream";
+        }
+
+        var bytes = System.IO.File.ReadAllBytes(pathToFile);
+
+        return File(bytes, contentType, Path.GetFileName(pathToFile));
+    }
+}
