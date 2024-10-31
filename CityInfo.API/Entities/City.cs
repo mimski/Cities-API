f4302b2a82b46cae05196dc19289d71767f2f1db
mimski <maria.bg.1989@gmail.com>
2024-10-31 23:50:54 +0200

    creates City entity

---
+++ CityInfo.API/Entities/City.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities;
+
+public class City
+{
+    public City(string name)
+    {
+        this.Name = name;
+    }
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string? Description { get; set; }
+}
