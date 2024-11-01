da403a9aca2cab4f0bc429ee95ba3c8d322c0951
mimski <maria.bg.1989@gmail.com>
2024-11-01 19:27:32 +0200

    adds PointOfInterest entity

---
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.API.Entities;
+
+public class PointOfInterest
+{
+    public PointOfInterest(string name)
+    {
+        this.Name = name;
+    }
+
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    [ForeignKey("CityId")]
+    public City? City { get; set; }
+
+    public int CityId { get; set; }
+}
