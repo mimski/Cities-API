32b1afc59fd0af60c1bdf95845b3a94f880250c1
mimski <maria.bg.1989@gmail.com>
2024-11-03 22:13:29 +0200

    creates DbContext

---
+++ CityInfo.API/DbContexts/CityInfoContext.cs
@@ -0,0 +1,18 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts;
+
+public class CityInfoContext : DbContext
+{
+    public DbSet<City> Cities { get; set; }
+
+    public DbSet<PointOfInterest> PointOfInterests { get; set; }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseSqlite("connectionstring");
+
+    //    base.OnConfiguring(optionsBuilder);
+    //}
+}
