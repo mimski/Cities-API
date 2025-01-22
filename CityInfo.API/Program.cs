4f40ea9dfbfbed6f70047ae993812d768c4f8e21
mimski <maria.bg.1989@gmail.com>
2025-01-22 18:36:11 +0200

    suporting versioning

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using CityInfo.API;
 using CityInfo.API.DbContexts;
 using CityInfo.API.Services;
@@ -75,6 +76,13 @@ builder.Services.AddAuthorization(options =>
         policy.RequireClaim("city", "Antwerp");
     });
 });
+
+builder.Services.AddApiVersioning(setupAction =>
+{
+    setupAction.ReportApiVersions = true;
+    setupAction.AssumeDefaultVersionWhenUnspecified = true;
+    setupAction.DefaultApiVersion = new ApiVersion(1, 0);
+}).AddMvc();
 
 var app = builder.Build();
 
