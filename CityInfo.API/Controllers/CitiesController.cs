dadffb6b1981d0c67bfb998adcb88696cae4a2a1
mimski <maria.bg.1989@gmail.com>
2025-01-22 18:47:40 +0200

    supporting versioned routes

--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -9,7 +9,7 @@ namespace CityInfo.API.Controllers;
 
 [ApiController]
 [Authorize]
-[Route("api/cities")]
+[Route("api/v{version:apiVersion}/cities")]
 [ApiVersion(1)]
 [ApiVersion(2)]
 public class CitiesController : ControllerBase
